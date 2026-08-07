86092a5854644144191a09a6a1eac9b07a399231
Nguyen Minh Duc <nguyenminhduc07012007@gmail.com>
2026-08-07 16:58:15 +0700

    Fix file path for Vercel

--- server.js
+++ server.js
@@ -2,16 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Phục vụ file index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(process.cwd(), 'index.html'));
 });
 
-// Chỉ mở cổng listen khi chạy Localhost trên máy
 if (process.env.NODE_ENV !== 'production') {
     const PORT = 3000;
     app.listen(PORT, '0.0.0.0', () => {
         console.log(`Server đang chạy tại http://localhost:${PORT}`);
     });
 }
+
 module.exports = app;
