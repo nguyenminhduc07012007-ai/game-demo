77eb2d1363c50b9e76e8a50199b796458b890b7e
Nguyen Minh Duc <nguyenminhduc07012007@gmail.com>
2026-08-07 16:43:27 +0700

    First commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Phục vụ file index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Chạy server lắng nghe tất cả các thiết bị trong mạng Wi-Fi
+const PORT = 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server đang chạy! Bạn có thể truy cập bằng IP port 3000.`);
+});
