5bbbc33b4a753bae49bd8220df5d60da1ce0af74
Nguyen Minh Duc <nguyenminhduc07012007@gmail.com>
2026-08-07 16:55:25 +0700

    Fix server exports

--- server.js
+++ server.js
@@ -7,8 +7,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Chạy server lắng nghe tất cả các thiết bị trong mạng Wi-Fi
-const PORT = 3000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server đang chạy! Bạn có thể truy cập bằng IP port 3000.`);
-});module.exports = app;
+// Chỉ mở cổng listen khi chạy Localhost trên máy
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = 3000;
+    app.listen(PORT, '0.0.0.0', () => {
+        console.log(`Server đang chạy tại http://localhost:${PORT}`);
+    });
+}
+module.exports = app;
